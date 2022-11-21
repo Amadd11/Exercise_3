543bcba7321c2c8b4917a3335eede3e85fc75eeb
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-11-21 15:42:53 +0700

    Add method Search & searches for the specified node

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -11,9 +11,31 @@ namespace Exercise_3
         /*creates Nodes for the circular nexted list*/
         public int rollNumber;
         public string name;
-        public Node next; 
+        public Node next;
     }
-    
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        /*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous =
+                current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*if the node is present at the end*/
+                return true;
+            else
+                return (false);/*returns false if the node is not found*/
+        }
+    }
     class Program
     {
         static void Main(string[] args)

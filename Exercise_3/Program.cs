80bf9c9b663668cf9110db00005d4b6d3d18d400
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-11-21 15:49:43 +0700

    Add traverse all the node of the list

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -42,6 +42,25 @@ namespace Exercise_3
             else
                 return false ;
         }
+
+        public void traverse()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecords in the list are: \n");
+                Node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + " " +
+                        currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + " " + LAST.name + "\n");
+            }
+        }
     }
     class Program
     {
